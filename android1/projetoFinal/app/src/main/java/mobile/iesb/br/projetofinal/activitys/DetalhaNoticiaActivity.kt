fa4a54e49e13d85f74357e2f6ad43b92c1f06e88
evertonnogueira <evertonnp10@gmail.com>
2018-05-10 19:03:09 -0300

    Listagem e detalhamento funcionando

--- android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/DetalhaNoticiaActivity.kt
+++ android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/DetalhaNoticiaActivity.kt
@@ -1,26 +1,22 @@
 package mobile.iesb.br.projetofinal.activitys
 
-import android.arch.persistence.room.Room
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.*
+import android.widget.ImageView
+import android.widget.TextView
 import mobile.iesb.br.projetofinal.R
-import mobile.iesb.br.projetofinal.dao.AppDatabase
 import mobile.iesb.br.projetofinal.entidade.Noticia
-import java.text.SimpleDateFormat
 
 class DetalhaNoticiaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalha_noticia)
-
-
+        var item = intent.extras.get("itemSelecionado") as Noticia
+        findViewById<ImageView>(R.id.imageViewNoticia).setImageBitmap(item.retornaBitMapImage())
+        findViewById<TextView>(R.id.tituloNoticia).text = item.titulo
+        findViewById<TextView>(R.id.corpoNoticia).text = item.texto
+        findViewById<TextView>(R.id.dataNoticia).text = item.getDataString()
     }
 
     public override fun onDestroy() {
