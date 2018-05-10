64e362ee601dfda8f39f64a1d6fbead965bcf442
Douglas Queiroz <douglasqueirozalmeida@gmail.com>
2018-05-10 19:57:47 -0300

    ajuste gerais

--- android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/HomeActivity.kt
+++ android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/HomeActivity.kt
@@ -5,13 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.*
 import mobile.iesb.br.projetofinal.R
 import mobile.iesb.br.projetofinal.dao.AppDatabase
 import mobile.iesb.br.projetofinal.entidade.Noticia
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -26,8 +25,8 @@ class HomeActivity : AppCompatActivity() {
                 AppDatabase::class.java, "room-database"
         ).allowMainThreadQueries().build()
 
-        var noticias = db?.noticiaDao()?.findAll()
-        noticias = noticias?.sortedByDescending { it.uid }
+
+        var noticias = cadastraNoticia()
 
         val listView = findViewById<ListView>(R.id.listNoticias)
         listView.adapter = NoticiaListAdapter(this, noticias!!)
@@ -36,14 +35,54 @@ class HomeActivity : AppCompatActivity() {
             myIntent.putExtra("itemSelecionado", adapterView.getItemAtPosition(position) as Noticia)
             startActivity(myIntent)
         }
+
+
     }
 
     public override fun onDestroy() {
         super.onDestroy()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun cadastraNoticia(): List<Noticia>? {
+        var noticias = db?.noticiaDao()?.findAll()
+
+        if (noticias?.size == 0) {
+
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo1", Date(), "Descricao Titulo1", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo2", Date(), "Descricao Titulo2", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo3", Date(), "Descricao Titulo3", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo4", Date(), "Descricao Titulo4", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo5", Date(), "Descricao Titulo5", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo6", Date(), "Descricao Titulo6", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo7", Date(), "Descricao Titulo7", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo8", Date(), "Descricao Titulo8", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo9", Date(), "Descricao Titulo9", ""))
+            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo10", Date(), "Descricao Titulo10", ""))
+
+            noticias = db?.noticiaDao()?.findAll()
+        }
+
+        return noticias?.sortedByDescending { it.uid }
+    }
 }
 
-private class NoticiaListAdapter(paramContexto: Context, paramNoticias: List<Noticia>) : BaseAdapter()  {
+private class NoticiaListAdapter(paramContexto: Context, paramNoticias: List<Noticia>) : BaseAdapter() {
 
     private val contexto: Context
     private var noticias: List<Noticia>
@@ -67,7 +106,7 @@ private class NoticiaListAdapter(paramContexto: Context, paramNoticias: List<Not
 
     override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
         val layoutInflater = LayoutInflater.from(contexto)
-        val noticiaRow = layoutInflater.inflate(R.layout.content_home, viewGroup,false)
+        val noticiaRow = layoutInflater.inflate(R.layout.content_home, viewGroup, false)
 
         val tituloTextView = noticiaRow.findViewById<TextView>(R.id.textViewTituloNoticia)
         tituloTextView.text = noticias.get(position).titulo
@@ -78,7 +117,7 @@ private class NoticiaListAdapter(paramContexto: Context, paramNoticias: List<Not
         val descricaotextView = noticiaRow.findViewById<TextView>(R.id.textViewTextoNoticia)
         descricaotextView.text = noticias.get(position).texto
 
-        val imageViewNoticia= noticiaRow.findViewById<ImageView>(R.id.imageViewImagemNoticia)
+        val imageViewNoticia = noticiaRow.findViewById<ImageView>(R.id.imageViewImagemNoticia)
         imageViewNoticia.setImageBitmap(noticias.get(position).retornaBitMapImage())
 
         return noticiaRow
--- android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/MainActivity.kt
+++ android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/MainActivity.kt
@@ -4,18 +4,14 @@ import android.arch.persistence.room.Room
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import mobile.iesb.br.projetofinal.R
 import mobile.iesb.br.projetofinal.dao.AppDatabase
-import mobile.iesb.br.projetofinal.entidade.Noticia
 import mobile.iesb.br.projetofinal.entidade.Usuario
 import mobile.iesb.br.projetofinal.util.ValidaUtil
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         var esqueceuSenha = findViewById<TextView>(R.id.textViewEsqueceuSenha)
 
-        esqueceuSenha.setOnClickListener{ view ->
+        esqueceuSenha.setOnClickListener { view ->
             val myIntent = Intent(this, EsqueceuSenhaActivity::class.java)
             startActivity(myIntent)
         }
@@ -52,35 +48,17 @@ class MainActivity : AppCompatActivity() {
                 applicationContext,
                 AppDatabase::class.java, "room-database"
         ).allowMainThreadQueries().build()
-//
-//
-//        cadastraNoticia()
+
         cadastraUsuario()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun isUsuarioValido() : Boolean {
+    private fun isUsuarioValido(): Boolean {
         var email = findViewById<EditText>(R.id.editTextEmailRecuperaSenha)
         var senha = findViewById<EditText>(R.id.editTextSenha)
 
         var usuario = db?.usuarioDao()?.findByEmailSenha(email.text.toString(), senha.text.toString())
 
-        if(usuario == null) {
+        if (usuario == null) {
 
             Toast.makeText(applicationContext, "Dados Incorretos", Toast.LENGTH_LONG).show()
             return false
@@ -89,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun validaInputs() : Boolean {
+    private fun validaInputs(): Boolean {
         var email = findViewById<EditText>(R.id.editTextEmailRecuperaSenha)
         var senha = findViewById<EditText>(R.id.editTextSenha)
 
@@ -102,35 +80,12 @@ class MainActivity : AppCompatActivity() {
     private fun cadastraUsuario() {
         var email = "admin@admin.com"
         var senha = "senha"
-        var usuarioAdmin = db?.usuarioDao()?.findByEmailSenha(email,senha)
+        var usuarioAdmin = db?.usuarioDao()?.findByEmailSenha(email, senha)
 
-        if(usuarioAdmin == null) {
+        if (usuarioAdmin == null) {
             db?.usuarioDao()?.insertUsuario(Usuario(0, "admin", email, "", senha, 0, 6199999999))
         }
     }
 
-    private fun cadastraNoticia() {
-        var noticias = db?.noticiaDao()?.findAll()
 
-        if (noticias?.size == 0) {
-
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo1", Date(), "Descricao Titulo1", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo2", Date(), "Descricao Titulo2", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo3", Date(), "Descricao Titulo3", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo4", Date(), "Descricao Titulo4", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo5", Date(), "Descricao Titulo5", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo6", Date(), "Descricao Titulo6", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo7", Date(), "Descricao Titulo7", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo8", Date(), "Descricao Titulo8", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo9", Date(), "Descricao Titulo9", ""))
-            db?.noticiaDao()?.insertNoticia(Noticia(0, "Titulo10", Date(), "Descricao Titulo10", ""))
-
-            noticias = db?.noticiaDao()?.findAll()
-        }
-
-        noticias = noticias?.sortedByDescending { it.uid }
-
-        Toast.makeText(applicationContext, noticias.toString(), Toast.LENGTH_LONG).show()
-
-    }
 }
