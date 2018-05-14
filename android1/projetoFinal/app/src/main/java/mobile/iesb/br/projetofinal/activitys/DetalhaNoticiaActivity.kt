f866f6c881d2dbe5eace679f8145d99bf42bacfa
Douglas Queiroz <douglasqueirozalmeida@gmail.com>
2018-05-14 16:03:07 -0300

    ajuste gerais

--- android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/DetalhaNoticiaActivity.kt
+++ android1/projetoFinal/app/src/main/java/mobile/iesb/br/projetofinal/activitys/DetalhaNoticiaActivity.kt
@@ -1,8 +1,8 @@
 package mobile.iesb.br.projetofinal.activitys
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -19,6 +19,7 @@ class DetalhaNoticiaActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.tituloNoticia).text = item.titulo
         findViewById<TextView>(R.id.corpoNoticia).text = item.texto
         findViewById<TextView>(R.id.dataNoticia).text = item.getDataString()
+        findViewById<TextView>(R.id.corpoNoticia).movementMethod = ScrollingMovementMethod()
 
 
         var voltar = findViewById<Button>(R.id.buttonVoltar)
